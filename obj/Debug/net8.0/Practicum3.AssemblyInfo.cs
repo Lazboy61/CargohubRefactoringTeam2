bc594a2796cb8b7f187ff9bb79b37e7d14260133
jvp010 <121518493+jvp010@users.noreply.github.com>
2024-10-19 22:42:16 +0200

    first branch

--- obj/Debug/net8.0/Practicum3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practicum3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practicum3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6b69848f64283e7d7f3bcda68049ccd16370bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4dfe2a447a57546fbf175d1c25402c6c15bb12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practicum3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practicum3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
