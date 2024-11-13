38abfcd7b8a3c60343a400328f5a709d3fd170a9
Al-Miraj <khadija.arkhouch@outlook.com>
2024-11-13 15:41:37 +0100

    VALDIER: added tests, a seeder and modified gitignore. installed more packages

---
+++ seeder.cs
@@ -0,0 +1,93 @@
+// // dit is om seeders te maken
+// // dit is als prep om unit testen te doen met data die al bereikbaar is en dat je niks handmatig hoeft te doen na het schrijven
+// // van een unit test
+// // eigenlijk niet nodig omdat inmemory DB dit ook kan doen lol
+// public static class Seeder{
+    
+//     public  static ItemGroup[] LoadItemGroups(){
+//         ItemGroup[] itemGroups =[
+//         new ItemGroup{
+//             Id = 0,
+//             Name = "seedername1",
+//             Description = "description1"
+//         },
+//         new ItemGroup{
+//             Id = 1,
+//             Name = "seedername1",
+//             Description = "description1"
+//         },
+//         new ItemGroup{
+//             Id = 2,
+//             Name = "seedername1",
+//             Description = "description1"
+//         },
+//         new ItemGroup{
+//             Id = 3,
+//             Name = "seedername1",
+//             Description = "description1"
+//         }
+//         ];
+//         return itemGroups;
+//     }
+//     public static Client[] LoadClients()
+//     {
+//         Client[] clients =[
+//         new Client{
+//             Id = 0,
+//             Name = "seedername",
+//             Address = "Address",
+//             City = "City1",
+//             ZipCode = "ZipCode",
+//             Province ="Province",
+//             Country= "Country",
+//             ContactName  = "ContactName",
+//             ContactPhone = "ContactPhone",
+//             ContactEmail = "ContactEmail"
+//         },
+//         new Client{
+//             Id = 1,
+//             Name = "seedername",
+//             Address = "Address",
+//             City = "City1",
+//             ZipCode = "ZipCode",
+//             Province ="Province",
+//             Country= "Country",
+//             ContactName  = "ContactName",
+//             ContactPhone = "ContactPhone",
+//             ContactEmail = "ContactEmail"
+//         },
+//         new Client{
+//             Id = 2,
+//             Name = "seedername",
+//             Address = "Address",
+//             City = "City1",
+//             ZipCode = "ZipCode",
+//             Province ="Province",
+//             Country= "Country",
+//             ContactName  = "ContactName",
+//             ContactPhone = "ContactPhone",
+//             ContactEmail = "ContactEmail"
+//         },
+//         new Client{
+//             Id = 3,
+//             Name = "seedername",
+//             Address = "Address",
+//             City = "City1",
+//             ZipCode = "ZipCode",
+//             Province ="Province",
+//             Country= "Country",
+//             ContactName  = "ContactName",
+//             ContactPhone = "ContactPhone",
+//             ContactEmail = "ContactEmail"
+//         }
+//         ];
+//         return clients;
+//     }
+// }
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+//                                                                       //
+//       Vergeet deze file Seeders zijn niet nodig bij het testen        //
+//                                                                       //
+//                                                                       //
+//////////////////////////////////////////////////////////////////////////
